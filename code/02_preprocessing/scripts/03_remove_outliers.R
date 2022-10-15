029cf3217773ced32a3cb5a78013b99882f41112
leawlb <lea.woelbert@gmail.com>
2022-10-15 18:48:35 +0200

    further structural changes and additions to config

--- code/02_preprocessing/scripts/03_remove_outliers.R
+++ code/02_preprocessing/scripts/03_remove_outliers.R
@@ -9,18 +9,25 @@ library(scuttle, quietly = TRUE)
 
 sce <- readRDS(file = snakemake@input[["sce_02"]])
 
-# calculate quality control dataframe
-qcdf <- perCellQCMetrics(sce)
-
 # set QC cutoffs below which cells are removed
 cutoff_sum <- snakemake@params[["cutoff_sum"]] 
 cutoff_detected <- snakemake@params[["cutoff_detected"]]
+cutoff_mitos <- snakemake@params[["cutoff_mitos"]]
 
+# get mitos
+mito_genes <- grep("mt-", rownames(sce))
+qcdf<- perCellQCMetrics(sce, 
+                        subsets=list(Mito=mito_genes)) # before outlier removal
+
+# set QC cutoffs below which cells are removed
 sum_out <- qcdf$sum < cutoff_sum 
-det_out <- qcdf$sum < cutoff_detected 
+det_out <- qcdf$detected < cutoff_detected 
+mito_out <- qcdf$subsets_Mito_percent > cutoff_mitos
 
 # remove all cells to discard and add info to qc
-remove_pos <- sum_out | det_out 
+remove_pos <- sum_out | det_out | mito_out
+qcdf$removed <- remove_pos
+
 sce <- sce[,!remove_pos]
 
 # save as new objects

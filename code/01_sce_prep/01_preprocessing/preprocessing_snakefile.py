651ed4b1a4e55fd25bce8b75dcca20afeb1bc18c
leawlb <lea.woelbert@gmail.com>
2023-06-20 19:49:33 +0200

    minor improvements, added age merges, deleted old env

--- code/01_sce_prep/01_preprocessing/preprocessing_snakefile.py
+++ code/01_sce_prep/01_preprocessing/preprocessing_snakefile.py
@@ -12,8 +12,8 @@ METADATA = pd.read_csv(config["metadata"]["table"])
 VALUES = config["values"]["preprocessing"]
 
 # specific data and report output paths
-OUTPUT_DAT = "/sce_objects/01_sce_prep/"
-OUTPUT_REP = "/reports/01_sce_prep/01_preprocessing/"
+OUTPUT_DAT = OUTPUT_BASE + "/sce_objects/01_sce_prep/"
+OUTPUT_REP = OUTPUT_BASE + "/reports/01_sce_prep/01_preprocessing/"
 
 def get_list(metadata, column):
   values = METADATA[column]
@@ -32,14 +32,14 @@ targets = []
 for s in species:
   for i in individuals:
     if s in i:
-      targets = targets + [OUTPUT_BASE + OUTPUT_DAT + "01_drop/" + s + "/sce_" + i + "-01"]
-      targets = targets + [OUTPUT_BASE + OUTPUT_DAT + "02_outl/" + s + "/sce_" + i + "-02"]
-      targets = targets + [OUTPUT_BASE + OUTPUT_DAT + "03_norm/" + s + "/sce_" + i + "-03"]
-      targets = targets + [OUTPUT_BASE + OUTPUT_DAT + "04_dimr/" + s + "/sce_" + i + "-04"]
-      targets = targets + [OUTPUT_BASE + OUTPUT_REP + s + "/preprocessing_sample_report_" + i + ".html"]
+      targets = targets + [OUTPUT_DAT + "01_drop/" + s + "/sce_" + i + "-01"]
+      targets = targets + [OUTPUT_DAT + "02_outl/" + s + "/sce_" + i + "-02"]
+      targets = targets + [OUTPUT_DAT + "03_norm/" + s + "/sce_" + i + "-03"]
+      targets = targets + [OUTPUT_DAT + "04_dimr/" + s + "/sce_" + i + "-04"]
+      targets = targets + [OUTPUT_REP + s + "/preprocessing_sample_report_" + i + ".html"]
       
 if config["run_preprocessing_summary"]:
-  targets = targets + [OUTPUT_BASE + OUTPUT_REP + "preprocessing_summary.html"]
+  targets = targets + [OUTPUT_REP + "preprocessing_summary.html"]
 
 # local execution of non-demanding rules
 localrules: all  
@@ -56,7 +56,7 @@ rule remove_droplets:
     input: 
         sce_input = OUTPUT_BASE + config["paths"]["cellranger_output"] + "/{species}/sce_{individual}-01"
     output:
-        sce_01 = OUTPUT_BASE + OUTPUT_DAT + "01_drop/{species}/sce_{individual}-01"
+        sce_01 = OUTPUT_DAT + "01_drop/{species}/sce_{individual}-01"
     params:
         cutoff_umis = VALUES["cutoff_umis"],
         cutoff_doublets = VALUES["cutoff_doublets"]
@@ -68,7 +68,7 @@ rule remove_outliers:
     input: 
         sce_01 = rules.remove_droplets.output
     output:
-        sce_02 = OUTPUT_BASE + OUTPUT_DAT + "02_outl/{species}/sce_{individual}-02"
+        sce_02 = OUTPUT_DAT + "02_outl/{species}/sce_{individual}-02"
     params:
         cutoff_sum = VALUES["cutoff_sum"],
         cutoff_detected = VALUES["cutoff_detected"],
@@ -81,7 +81,7 @@ rule normalize_expr:
     input: 
         sce_02 = rules.remove_outliers.output
     output:
-        sce_03 = OUTPUT_BASE + OUTPUT_DAT + "03_norm/{species}/sce_{individual}-03"
+        sce_03 = OUTPUT_DAT + "03_norm/{species}/sce_{individual}-03"
     script:
         "scripts/03_normalize_expr.R"
         
@@ -90,7 +90,7 @@ rule reduce_dims:
     input: 
         sce_03 = rules.normalize_expr.output
     output:
-        sce_04 = OUTPUT_BASE + OUTPUT_DAT + "04_dimr/{species}/sce_{individual}-04" 
+        sce_04 = OUTPUT_DAT + "04_dimr/{species}/sce_{individual}-04" 
     params:
         nr_hvgs = VALUES["nr_hvgs"]
     script:
@@ -101,13 +101,13 @@ rule reduce_dims:
 
 rule make_sample_reports:
     input:
-        sce_input = OUTPUT_BASE + config["paths"]["cellranger_output"] + "/{species}/sce_{individual}-01",
+        sce_input = config["paths"]["cellranger_output"] + "/{species}/sce_{individual}-01",
         sce_01 = rules.remove_droplets.output,
         sce_02 = rules.remove_outliers.output,
         sce_03 = rules.normalize_expr.output,
         sce_04 = rules.reduce_dims.output
     output:
-        OUTPUT_BASE + OUTPUT_REP + "{species}/preprocessing_sample_report_{individual}.html"
+        OUTPUT_REP + "{species}/preprocessing_sample_report_{individual}.html"
     params:
         cutoff_umis = VALUES["cutoff_umis"],
         cutoff_doublets = VALUES["cutoff_doublets"],
@@ -129,9 +129,9 @@ print(config["run_preprocessing_summary"])
 if config["run_preprocessing_summary"]:
   rule make_summary_report:
       input:
-          sce_01_path = OUTPUT_BASE + OUTPUT_DAT + "01_drop/"
+          sce_01_path = OUTPUT_DAT + "01_drop/"
       output:
-          OUTPUT_BASE + OUTPUT_REP + "preprocessing_summary.html"
+          OUTPUT_REP + "preprocessing_summary.html"
       params:
           cutoff_sum = VALUES["cutoff_sum"],
           cutoff_detected = VALUES["cutoff_detected"],
--- code/01_sce_prep/03_integration/integration_snakefile.py
+++ code/01_sce_prep/03_integration/integration_snakefile.py
@@ -10,8 +10,8 @@ METADATA = pd.read_csv(config["metadata"]["table"])
 TABLES_PATH = config["metadata"]["color_tables"]
 
 # specific data and report output paths
-OUTPUT_DAT = "/sce_objects/01_sce_prep/"
-OUTPUT_REP = "/reports/01_sce_prep/03_integration/"
+OUTPUT_DAT = OUTPUT_BASE + "/sce_objects/01_sce_prep/"
+OUTPUT_REP = OUTPUT_BASE + "/reports/01_sce_prep/03_integration/"
 
 #-------------------------------------------------------------------------------
 
@@ -25,16 +25,23 @@ def get_list(metadata, column):
 species = get_list(metadata = METADATA, column = "Species_ID")
 individuals = get_list(metadata = METADATA, column = "Object_ID")
 fractions = get_list(metadata = METADATA, column = "Fraction_ID")
+ages = get_list(metadata = METADATA, column = "Age_ID")
 
+targets = []
 for f in fractions:
-  targets = [OUTPUT_BASE + OUTPUT_DAT + "06_mrge/fractions/sce_" + f + "-06"]
-  targets = targets + [OUTPUT_BASE + OUTPUT_DAT + "06_mrge/species/sce_" + s + "_" + f + "-06" for s in species]
-  targets = targets + [OUTPUT_BASE + OUTPUT_REP + "species/integration_report_" + s + "_" + f + ".html" for s in species]
+  targets = targets + [OUTPUT_DAT + "06_mrge/fractions/sce_" + f + "-06"]
+  targets = targets + [OUTPUT_REP + "fractions/integration_report_" + f + ".html"]
+  
+for f in fractions:
+  for s in species:
+    targets = targets + [OUTPUT_DAT + "06_mrge/species/sce_" + s + "_" + f + "-06"]
+    targets = targets + [OUTPUT_REP + "species/integration_report_" + s + "_" + f + ".html"]
 
-targets = targets + [OUTPUT_BASE + OUTPUT_REP + "fractions/integration_report_hsc.html"]
-targets = targets + [OUTPUT_BASE + OUTPUT_REP + "fractions/integration_report_str.html"]
-targets = targets + [OUTPUT_BASE + OUTPUT_REP + "species/integration_report_" + s + "_hsc.html" for s in species]
-targets = targets + [OUTPUT_BASE + OUTPUT_REP + "species/integration_report_" + s + "_str.html" for s in species]
+for s in species:
+  for a in ages:
+    print(a)
+    targets = targets + [OUTPUT_DAT + "06_mrge/ages/sce_" + s + "_" + a + "-06"]
+    targets = targets + [OUTPUT_REP + "ages/integration_report_" + s + "_" + a + ".html"]
 
 print(targets)
 #-------------------------------------------------------------------------------
@@ -46,13 +53,13 @@ rule all: # must contain all possible output paths from all rules
     input:
         targets
         
-# Merge fractions 
+# Merge fractions (for annotation)
 rule merge_datasets_fractions:
     input: 
-        sce_05_path = expand(OUTPUT_BASE + OUTPUT_DAT + "05_rfan/{s}/", s = species)
+        sce_05_path = expand(OUTPUT_DAT + "05_rfan/{s}/", s = species)
     output:
-        sce_06_hsc = OUTPUT_BASE + OUTPUT_DAT + "06_mrge/fractions/sce_hsc-06",
-        sce_06_str = OUTPUT_BASE + OUTPUT_DAT + "06_mrge/fractions/sce_str-06"
+        sce_06_hsc = OUTPUT_DAT + "06_mrge/fractions/sce_hsc-06",
+        sce_06_str = OUTPUT_DAT + "06_mrge/fractions/sce_str-06"
     params:
         individuals = individuals,
         species = species,
@@ -61,13 +68,13 @@ rule merge_datasets_fractions:
     script:
         "scripts/06_merge_datasets_fractions.R" 
         
-# Merge species (same as above but for each species separately)
+# Merge species (same as above but for each species separately) (for NMF)
 rule merge_datasets_species:
     input: 
-        sce_05_path = OUTPUT_BASE + OUTPUT_DAT + "05_rfan/{species}/"
+        sce_05_path = OUTPUT_DAT + "05_rfan/{species}/"
     output:
-        sce_06_hsc = OUTPUT_BASE + OUTPUT_DAT + "06_mrge/species/sce_{species}_hsc-06",
-        sce_06_str = OUTPUT_BASE + OUTPUT_DAT + "06_mrge/species/sce_{species}_str-06"
+        sce_06_hsc = OUTPUT_DAT + "06_mrge/species/sce_{species}_hsc-06",
+        sce_06_str = OUTPUT_DAT + "06_mrge/species/sce_{species}_str-06"
     params:
         individuals = individuals,
         species = species,
@@ -75,13 +82,31 @@ rule merge_datasets_species:
         nr_hvgs = config["values"]["preprocessing"]["nr_hvgs"]
     script:
         "scripts/06_merge_datasets_species.R" 
+        
+# Merge ages (same as above but for each age separately) (for CCI)
+rule merge_datasets_ages:
+    input: 
+        sce_05_path = OUTPUT_DAT + "05_rfan/{species}/"
+    output:
+        sce_06_old = OUTPUT_DAT + "06_mrge/ages/sce_{species}_old-06",
+        sce_06_yng = OUTPUT_DAT + "06_mrge/ages/sce_{species}_yng-06"
+    params:
+        individuals = individuals,
+        species = species,
+        samples_to_remove = config["samples_to_remove"],
+        nr_hvgs = config["values"]["preprocessing"]["nr_hvgs"],
+        ages = ages
+    script:
+        "scripts/06_merge_datasets_ages.R" 
  
+#-------------------------------------------------------------------------------
+
 # Summary for fractions
 rule make_report_fractions:
     input: 
-        sce_06 = OUTPUT_BASE + OUTPUT_DAT + "06_mrge/fractions/sce_{fraction}-06"
+        sce_06 = OUTPUT_DAT + "06_mrge/fractions/sce_{fraction}-06"
     output:
-        OUTPUT_BASE + OUTPUT_REP + "fractions/integration_report_{fraction}.html"
+        OUTPUT_REP + "fractions/integration_report_{fraction}.html"
     params:
         color_tables = TABLES_PATH
     script:
@@ -90,11 +115,22 @@ rule make_report_fractions:
 # Summary for species
 rule make_report_species:
     input: 
-        sce_06 = OUTPUT_BASE + OUTPUT_DAT + "06_mrge/species/sce_{species}_{fraction}-06"
+        sce_06 = OUTPUT_DAT + "06_mrge/species/sce_{species}_{fraction}-06"
     output:
-        OUTPUT_BASE + OUTPUT_REP + "species/integration_report_{species}_{fraction}.html"
+        OUTPUT_REP + "species/integration_report_{species}_{fraction}.html"
     params:
         color_tables = TABLES_PATH
     script:
         "integration_report_species.Rmd" 
+        
+# Summary for ages
+rule make_report_ages:
+    input: 
+        sce_06 = OUTPUT_DAT + "06_mrge/ages/sce_{species}_{age}-06"
+    output:
+        OUTPUT_REP + "ages/integration_report_{species}_{age}.html"
+    params:
+        color_tables = TABLES_PATH
+    script:
+        "integration_report_ages.Rmd" 
         

919e998f5afb4d8d1056575b9395f39932712d0a
leawlb <lea.woelbert@gmail.com>
2023-07-06 16:17:02 +0200

    streamlined 02_anno

---
+++ code/02_sce_anno/01_clustering/clustering_snakefile.py
@@ -0,0 +1,211 @@
+#!/bin/python 
+
+"""
+Batch correction, clustering and cluster annotation
+
+This is the version for tested pipelines. 
+For testing pipelines with other methods see 02_sce_anno_TEST
+
+"""
+#-------------------------------------------------------------------------------
+
+import pandas as pd
+
+# paths from config
+OUTPUT_BASE = config["paths"]["output_dir"]
+TABLES_PATH = config["metadata"]["color_tables"]
+
+# objects from config
+METADATA = pd.read_csv(config["metadata"]["table"])
+BATCH_USE = config["values"]["batch_correction"]["batch_use_fractions"] # which colData to use as batch
+# seurat kept for QC sake but not used for downstream analysis
+SEURAT_RED = config["values"]["batch_correction"]["seurat_reduction"] # which seurat method to use
+
+# specific data and report output paths
+OUTPUT_DAT = OUTPUT_BASE + "/sce_objects/02_sce_anno/"
+OUTPUT_REP = OUTPUT_BASE + "/reports/02_sce_anno/"
+
+# cannot take from metadata since cluster number is dependent on fraction
+clusters_hsc = list(range(1,14))
+clusters_hsc = list(map(str,clusters_hsc))
+
+clusters_str = list(range(1,19))
+clusters_str = list(map(str,clusters_str))
+
+#-------------------------------------------------------------------------------
+
+def get_list(metadata, column):
+  values = METADATA[column]
+  values = values.drop_duplicates()
+  values = values.squeeze()
+  values = values.tolist()
+  return(values)
+  
+fractions = get_list(metadata = METADATA, column = "Fraction_ID")
+
+targets = []
+for f in fractions:
+  targets = targets + [OUTPUT_DAT + "01_mnnc/sce_" + f + "_" + BATCH_USE + "-01"]
+  targets = targets + [OUTPUT_DAT + "02_clst/louvn_clust/sce_" + f + "-02"]
+  targets = targets + [OUTPUT_REP + "01_batch_correction/batch_correction_report_" + f + "_" + BATCH_USE + ".html"]
+  targets = targets + [OUTPUT_REP + "02_clustering/clustering_report_full_" + f +".html"]
+  targets = targets + [OUTPUT_DAT + "04_annc/01_markers/markers_" + f]
+  targets = targets + [OUTPUT_DAT + "04_annc/02_goan/go_" + f]
+  targets = targets + [OUTPUT_DAT + "04_annc/03_sce/sce_" + f + "-04"]
+
+for c in clusters_hsc:
+  targets = targets + [OUTPUT_DAT + "03_sepd/sce_hsc_cluster_" + c + "-sep"]
+  targets = targets + [OUTPUT_REP + "03_anno_clusters/annotation_hsc_cluster_" + c + ".html" ] 
+
+for c in clusters_str:
+  targets = targets + [OUTPUT_DAT + "03_sepd/sce_str_cluster_" + c + "-sep"]
+  targets = targets + [OUTPUT_REP + "03_anno_clusters/annotation_str_cluster_" + c + ".html"] 
+
+#-------------------------------------------------------------------------------
+
+localrules: all  
+
+# define rules
+rule all: # must contain all possible output paths from all rules
+    input:
+        targets
+ 
+#-------------------------------------------------------------------------------
+"""
+Info on batch correction methods from:
+Luecken et al. "Benchmarking atlas-level data integration in single-cell 
+genomics", Nat Met 2022
+Tran, Ang, Chevrier, Zhang et al. "A benchmark of batch effect correction 
+methods for single-cell RNA sequencing data", Genome Biology 2020
+
+Batch correction using MNNcorrect:
+
+"""
+rule run_mnncorrect:
+    input:
+        sce_input = OUTPUT_BASE + "/sce_objects/01_sce_prep/08_mrge/fractions/sce_{fraction}-08"
+    output:
+        sce_output = OUTPUT_DAT + "01_mnnc/sce_{fraction}_" + BATCH_USE + "-01"
+    params:
+        batch_use = BATCH_USE,
+        nr_hvgs_batch_correction = config["values"]["batch_correction"]["nr_hvgs_batch_correction"], 
+        nr_hvgs = config["values"]["preprocessing"]["nr_hvgs"],
+        seeds_umap = config["values"]["batch_correction"]["seeds_umap"]
+    script:
+        "scripts/01_mnncorrect.R"
+        
+          
+"""
+Make batch correction report
+"""
+
+rule make_batchcorrection_reports:
+    input:
+        sce_input_raw = OUTPUT_BASE + "/sce_objects/01_sce_prep/08_mrge/fractions/sce_{fraction}-08",
+        sce_input_corrected = rules.run_mnncorrect.output
+    output:
+        OUTPUT_REP + "01_batch_correction/batch_correction_report_{fraction}_" + BATCH_USE + ".html"
+    params:
+        batch_use = BATCH_USE,
+        color_tables = TABLES_PATH
+    script:
+        "batch_correction_report.Rmd" 
+  
+        
+#-------------------------------------------------------------------------------
+"""
+Louvain clustering
+"""
+
+rule louvain_clustering:
+    input: 
+        sce_input = rules.run_mnncorrect.output
+    output:
+        sce_output = OUTPUT_DAT + "02_clst/louvn_clust/sce_{fraction}-02"
+    params:
+        k_louvain = config["values"]["clustering"]["k_louvain"]
+    script:
+        "scripts/02_louvain_clustering.R"
+  
+"""
+Make clustering reports 
+"""      
+        
+rule make_clustering_report:
+    input:
+        sce_input = rules.run_mnncorrect.output,
+        sce_l = rules.louvain_clustering.output
+    params:
+        color_tables = TABLES_PATH
+    output:
+        OUTPUT_REP + "02_clustering/clustering_report_full_{fraction}.html"
+    script:
+        "clustering_report_full.Rmd"
+        
+#-------------------------------------------------------------------------------
+"""
+Dummy rule to allow using clusters as wildcard
+"""
+
+output = expand(OUTPUT_DAT + "03_sepd/sce_hsc_cluster_{cluster}-sep", cluster = clusters_hsc)
+output = output + expand(OUTPUT_DAT + "03_sepd/sce_str_cluster_{cluster}-sep", cluster = clusters_str)
+rule separate_sce:
+    input: 
+        sce_input = expand(OUTPUT_DAT + "02_clst/louvn_clust/sce_{fraction}-02", fraction = fractions)
+    output:
+        sce_output = output
+    script:
+        "scripts/03_separate_dummy.R"
+        
+        
+#-------------------------------------------------------------------------------
+ 
+"""
+Clustering Annotation
+"""
+
+# get marker genes for each cluster 
+rule find_markers:
+    input: 
+        sce_input = rules.louvain_clustering.output
+    output:
+        markers = OUTPUT_DAT + "04_annc/01_markers/markers_{fraction}"
+    params:
+        nr_hvgs = config["values"]["preprocessing"]["nr_hvgs"],
+    script:
+        "scripts/04_markers_clusters.R"
+
+# perform preliminary GO analysis for overview (Very basic)
+rule go_analysis:
+    input: 
+        markers = rules.find_markers.output
+    output:
+        go = OUTPUT_DAT + "04_annc/02_goan/go_{fraction}"
+    script:
+        "scripts/05_go_clusters.R"
+
+# report on marker gene expression and GO 
+rule make_report:
+    input: 
+        markers = rules.find_markers.output,
+        go = rules.go_analysis.output,
+        cluster_annotations = "cluster_annotations.txt",
+        sce_sep = OUTPUT_DAT + "03_sepd/sce_{fraction}_cluster_{cluster}-sep",
+        sce_input = rules.louvain_clustering.output,
+        gene_list_all = "gene_list.txt"
+    output:
+        OUTPUT_REP + "03_anno_clusters/annotation_{fraction}_cluster_{cluster}.html"
+    params:
+        color_tables = TABLES_PATH
+    script:
+        "cluster_anno_report.Rmd"
+        
+# assign annotation labels informed from report
+rule assign_annotation:
+    input:
+        sce_input = rules.louvain_clustering.output,
+        cluster_annotations = "cluster_annotations.txt"
+    output:      
+        sce_output = OUTPUT_DAT + "04_annc/03_sce/sce_{fraction}-04"
+    script:
+        "scripts/06_anno_clusters.R"
---
+++ code/02_sce_anno/01_clustering/scripts/01_mnncorrect.R
@@ -0,0 +1,89 @@
+#-------------------------------------------------------------------------------
+
+library(DropletUtils, quietly = TRUE)
+library(batchelor, quietly = TRUE)
+source(file = "../../source/sce_functions.R")
+set.seed(37)
+
+#-------------------------------------------------------------------------------
+
+# load object and prepare batch vector for correctExperiments() + preparation
+sce <- readRDS(file = snakemake@input[["sce_input"]])
+
+# load parameters from config
+# which colData to use as batch
+batch_use <- snakemake@params[["batch_use"]]
+batch_pos <- which(colnames(colData(sce)) == batch_use)
+# how many hvgs for batch correction
+nr_hvgs_batch_correction <- snakemake@params[["nr_hvgs_batch_correction"]] 
+nr_hvgs <- snakemake@params[["nr_hvgs"]]
+
+# rename or copy the already existant Reduceddims and assays for testing
+reducedDimNames(sce)[grep("PCA", reducedDimNames(sce))] <- "PCA_before_BC"
+reducedDimNames(sce)[grep("UMAP", reducedDimNames(sce))] <- "UMAP_before_BC"
+assays(sce)$counts_before_BC <- assays(sce)$counts
+assays(sce)$logcounts_before_BC <- assays(sce)$logcounts
+
+# add info on batch to SCE
+sce$Batch_type_used <- rep(batch_use, ncol(sce))
+
+#-------------------------------------------------------------------------------
+
+# fastMNN recommends use of MultiBatchNorm, output in logcounts
+renorm_sce <- multiBatchNorm(sce, batch = colData(sce)[,batch_pos])
+stopifnot(colnames(renorm_sce) == colnames(sce))
+assays(sce)$logcounts <- assays(renorm_sce)$logcounts
+
+print(sce)
+
+#-------------------------------------------------------------------------------
+
+# calculate hvgs 
+gene_var <- modelGeneVar(sce)
+hvgs_for_batch_correction <- getTopHVGs(gene_var, n = nr_hvgs_batch_correction)
+hvgs <- getTopHVGs(gene_var, n = nr_hvgs)
+
+#-------------------------------------------------------------------------------
+
+print("starting MNN")
+
+# correct using MultiBatchNorm values in logcounts assay
+sce_bc <- correctExperiments(sce,
+                             correct.all = TRUE,
+                             PARAM = FastMnnParam(cos.norm = FALSE), # since it was previously normalized
+                             batch = colData(sce)[,batch_pos],
+                             subset.row = hvgs_for_batch_correction, 
+                             assay.type = "logcounts") 
+sce_bc$Correction_method <- rep("FastMNN", ncol(sce_bc))
+
+print("done MNN")
+print(sce_bc)
+
+#-------------------------------------------------------------------------------
+  
+# rename it "PCA" because orig PCA is called PCA_before 
+# so all following functions use corrected PCA automatically
+reducedDimNames(sce_bc)[reducedDimNames(sce_bc) == "corrected"] <- "PCA"
+colnames(reducedDim(sce_bc, type = "PCA")) <- c(1:ncol(
+  reducedDim(sce_bc, type = "PCA")))
+colnames(reducedDim(sce_bc, type = "PCA")) <- paste0(
+  "PC", colnames(reducedDim(sce_bc, type = "PCA")))
+  
+# calculate UMAP based on corrected PCA
+# no new hvg determination because logcounts are not corrected
+seeds_umap <- snakemake@params[["seeds_umap"]]
+if(sce_bc$Fraction_ID[1] == "hsc"){
+  seed <- seeds_umap[["hsc"]]
+}else if(sce_bc$Fraction_ID[1] == "str"){
+  seed <- seeds_umap[["str"]]
+}
+set.seed(seed)
+print(seed)
+  
+sce_bc <- runUMAP(sce_bc, dimred = "PCA", subset_row = hvgs)
+
+warnings()
+print(sce_bc)
+
+#-------------------------------------------------------------------------------
+saveRDS(sce_bc, file = snakemake@output[["sce_output"]])

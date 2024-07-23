1f132803f11d21a97af59b28b2d5babf06dc5af6
leawlb <lea.woelbert@gmail.com>
2024-07-23 13:18:58 +0200

    improvement

--- code/02_sce_anno/01_clustering/scripts/02_louvain_clustering.R
+++ code/02_sce_anno/01_clustering/scripts/02_louvain_clustering.R
@@ -3,36 +3,48 @@
 
 library(scran, quietly = TRUE)
 library(igraph, quietly = TRUE)
-#library(bluster, quietly = TRUE) # for NNGraphParam()
 set.seed(37)
 
 #-------------------------------------------------------------------------------
+# objects and params 
+sce <- base::readRDS(file = snakemake@input[["sce_input"]])
 
-sce <- readRDS(file = snakemake@input[["sce_input"]])
 k_graph_list <- snakemake@params[["k_graph_list"]]
 resolution_louvain_list <- snakemake@params[["resolution_louvain_list"]]
 fraction_curr <-  snakemake@wildcards[["fraction"]]
 
 k_graph <- k_graph_list[[fraction_curr]]
 resolution_louvain <- resolution_louvain_list[[fraction_curr]]
+
 print(k_graph)
 print(resolution_louvain)
 
+#-------------------------------------------------------------------------------
+# get a graph of nearest neighbors
 graph <- scran::buildSNNGraph(sce, 
                               k = k_graph, 
                               use.dimred = "PCA", # corrected
                               type = "rank")
 
+# community detection
 clust <- igraph::cluster_louvain(graph, 
                                  resolution = resolution_louvain)
 
-sce$cluster_louvain <- as.character(clust$membership)
-sce$cluster_louvain <- factor(sce$cluster_louvain, 
-                              levels = sort(unique(sce$cluster_louvain))) 
-sce$k_louvain <- rep(k_graph, ncol(sce))
-sce$resolution_louvain <- rep(resolution_louvain, ncol(sce))
+#-------------------------------------------------------------------------------
+# add to sce object 
+sce$cluster_louvain <- clust$membership
+
+sce$cluster_louvain <- base::factor(
+  sce$cluster_louvain, 
+  levels = base::sort(base::unique(sce$cluster_louvain)))
+
+
+sce$k_graph <- base::rep(k_graph, ncol(sce))
+sce$resolution_louvain <- base::rep(resolution_louvain, ncol(sce))
 
 print(sce)
-saveRDS(sce, file = snakemake@output[["sce_output"]])
 
-sessionInfo()
+#-------------------------------------------------------------------------------
+base::saveRDS(sce, file = snakemake@output[["sce_output"]])
+
+utils::sessionInfo()

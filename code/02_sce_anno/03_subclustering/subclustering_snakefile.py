252f84c3987781316c2201ed3a9635c64420c602
leawlb <lea.woelbert@gmail.com>
2024-07-30 10:38:07 +0200

    implemented conditional run of long reports

--- code/02_sce_anno/03_subclustering/subclustering_snakefile.py
+++ code/02_sce_anno/03_subclustering/subclustering_snakefile.py
@@ -16,8 +16,12 @@ VALUES =  config["values"]["02_sce_anno"]
 GENE_LIST_CLUSTERS = config["base"] + config["metadata_paths"]["gene_list_clusters"]
 GENE_LIST_SUBCLUSTERING = config["base"] + config["metadata_paths"]["gene_list_subclustering"]
 GENE_LIST_DOTPLOT = config["base"] + config["metadata_paths"]["gene_list_dotplot"]
+GENE_LIST_DOTPLOT_SHORT = config["base"] + config["metadata_paths"]["gene_list_dotplot_short"]
 ANNO_SUBCLUSTERS = config["base"] + config["metadata_paths"]["annotation_subclusters"]
 ANNO_FINAL = config["base"] + config["metadata_paths"]["annotation_final"]
+
+RUN_MARKER_REPORTS = config["run_marker_reports"]
+
 
 #-------------------------------------------------------------------------------
 
@@ -49,25 +53,28 @@ subclusters_str = list(map(str,subclusters_str))
 targets = []
 for c in clusters_hsc:
   targets = targets + [OUTPUT_DAT + "/09_sbcl/sce_hsc_cluster_" + c + "-09"]
-  #targets = targets + [OUTPUT_REP + "/mclust/mclust_report_hsc_cluster_" + c + ".html"]
+  targets = targets + [OUTPUT_REP + "/mclust/mclust_report_hsc_cluster_" + c + ".html"]
 
 for c in clusters_str:
   targets = targets + [OUTPUT_DAT + "/09_sbcl/sce_str_cluster_" + c + "-09"]
-  #targets = targets + [OUTPUT_REP + "/mclust/mclust_report_str_cluster_" + c + ".html"]
+  targets = targets + [OUTPUT_REP + "/mclust/mclust_report_str_cluster_" + c + ".html"]
 
-# for f in fractions:
-#   targets = targets + [OUTPUT_DAT + "/10_anns/sce_" + f + "-10"]
-#   targets = targets + [OUTPUT_REP + "/results/results_report_" + f + ".html"]
+for f in fractions:
+  targets = targets + [OUTPUT_DAT + "/10_anns/sce_" + f + "-10"]
+  targets = targets + [OUTPUT_REP + "/results/results_report_" + f + "_dotplots.html"]
+  targets = targets + [OUTPUT_REP + "/results/results_report_" + f + "_umaps.html"]
 #   targets = targets + [OUTPUT_DAT + "/12_anqc/01_markers/markers_" + f]
 #   targets = targets + [OUTPUT_DAT + "/12_anqc/02_go/go_" + f]
 # 
 # for s in subclusters_hsc:
 #   targets = targets + [OUTPUT_DAT + "/11_sepc/hsc_subcluster_" + s + "-sep"]
-#   targets = targets + [OUTPUT_REP + "/markergenes/markergenes_report_hsc_subcluster_" + s + ".html"]
+#   if RUN_MARKER_REPORTS: 
+#     targets = targets + [OUTPUT_REP + "/markergenes/markergenes_report_hsc_subcluster_" + s + ".html"]
 #   
 # for s in subclusters_str:
 #   targets = targets + [OUTPUT_DAT + "/11_sepc/str_subcluster_" + s + "-sep"]
-#   targets = targets + [OUTPUT_REP + "/markergenes/markergenes_report_str_subcluster_" + s + ".html"]
+#   if RUN_MARKER_REPORTS:
+#     targets = targets + [OUTPUT_REP + "/markergenes/markergenes_report_str_subcluster_" + s + ".html"]
 
 #-------------------------------------------------------------------------------
 
@@ -134,23 +141,30 @@ rule add_subclusters:
 
 """
 # report annotation results of entire fraction
-# I take many plots from this script for "main figure drafts", so it is quite 
-# long and under continuous processing/editing 
 """
-rule make_subclustering_results_report:
+rule make_subclustering_results_report_dotplot:
     input:
         sce_input = rules.add_subclusters.output,
-        gene_list_subclustering = GENE_LIST_SUBCLUSTERING,
-        gene_list_dtplt = GENE_LIST_DOTPLOT,
-        genes_list_shared = OUTPUT_DAT + "/08_nres/PC_0.05_FC_1.5/res_{fraction}_cluster_shared"
+        gene_list_dtplt = GENE_LIST_DOTPLOT
+    params:
+        colors_path = COLORS,
+        colors = "../../source/colors.R"
+    output:
+        OUTPUT_REP + "/results/results_report_{fraction}_dotplots.html"
+    script:
+        "subclustering_results_report_dotplots.Rmd" 
+        
+rule make_subclustering_results_report_umaps:
+    input:
+        sce_input = rules.add_subclusters.output
     params:
         colors_path = COLORS,
         plotting = "../../source/plotting.R",
         colors = "../../source/colors.R"
     output:
-        OUTPUT_REP + "/results/results_report_{fraction}.html"
+        OUTPUT_REP + "/results/results_report_{fraction}_umaps.html"
     script:
-        "subclustering_results_report.Rmd" 
+        "subclustering_results_report_umaps.Rmd" 
 
 #-------------------------------------------------------------------------------
 

20c3467e273f2f795a180975ad0e2d5dee866525
leawlb <lea.woelbert@gmail.com>
2024-08-01 09:13:37 +0200

    improving scripts

--- code/03_sce_analysis/02_DESeq2_crossspecies/scripts/01_aggregate_convert.R
+++ code/03_sce_analysis/02_DESeq2_crossspecies/scripts/01_aggregate_convert.R
@@ -1,68 +1,82 @@
 #-------------------------------------------------------------------------------
 # aggregate SCE objects for each Sample, then convert to Deseq2 object
 
-library(DESeq2)
-library(scuttle)
+library(DESeq2, quietly = TRUE)
+library(scuttle, quietly = TRUE)
 set.seed(37)
 
 #-------------------------------------------------------------------------------
 
-sce <- readRDS(file = snakemake@input[["sce_input"]])
+sce <- base::readRDS(file = snakemake@input[["sce_input"]])
 
 stopifnot(!is.na(sce$annotation_cluster))
 stopifnot(!is.na(sce$cluster_louvain))
 
 #-------------------------------------------------------------------------------
-# aggregate all cells with the same combination of Object ID and cell type 
-# counts is the same assays as "counts_before_BC
+# aggregate all cells with the same combination of Object_ID (sample) and
+# cell type
+# using unaltered counts before batch correction
 agg <- scuttle::aggregateAcrossCells(
   sce, 
-  id=colData(sce)[,c("Object_ID", "celltypes")],
-  use.assay.type = "counts")
+  use.assay.type = "counts_before_BC",
+  id=colData(sce)[,c("Object_ID", "celltypes")])
 
 print(agg)
 
 #-------------------------------------------------------------------------------
 # for each cell type, put into a separate list item to perform analysis per ct
 # only keep relevant col data
+
 agg_list <- list()
-for(i in levels(agg$celltypes)){
-  agg_list[[i]] <- agg[,agg$celltypes==i]
+for(ct in levels(agg$celltypes)){
+  agg_list[[ct]] <- agg[,agg$celltypes==i]
   
-  agg_list[[i]]$batch <- factor(agg_list[[i]]$Batch_exp_day)
-  agg_list[[i]]$species <- factor(agg_list[[i]]$Species_ID)
-  agg_list[[i]]$sample <- factor(agg_list[[i]]$Object_ID)
-  agg_list[[i]]$age <- factor(agg_list[[i]]$Age_ID)
+  agg_list[[ct]]$batch <- factor(agg_list[[i]]$Batch_exp_day)
+  agg_list[[ct]]$species <- factor(agg_list[[i]]$Species_ID)
+  agg_list[[ct]]$sample <- factor(agg_list[[i]]$Object_ID)
+  agg_list[[ct]]$age <- factor(agg_list[[i]]$Age_ID)
   
-  colData(agg_list[[i]]) <- colData(agg_list[[i]])[,c("batch", "species", 
-                                                      "sample", "age", "ncells", 
-                                                      "Antibody_combination", 
-                                                      "Batch_sequencing")]
+  colData(agg_list[[ct]]) <- colData(agg_list[[ct]])[,c("batch",
+                                                        "species", 
+                                                        "sample",
+                                                        "age", 
+                                                        "ncells", 
+                                                        "Antibody_combination", 
+                                                        "Batch_sequencing")]
 }
 # each list item contains an object for each cell type, containing all 
 # samples from each species and age
 
 #-------------------------------------------------------------------------------
-# convert to DeSeq2 object (sequence in design is not too important for now)
-# "batch" and "age" are covariates, "species" the species to be tested
+# convert to DESeq2 object (sequence in design will be adjusted in 07)
+# "batch" and "age" are covariates, "condition" the condition to be tested 
+# = species
 
 dsq_list <- lapply(agg_list, function(agg){
+  
+  print(agg)
+  
   dsq <- DESeq2::DESeqDataSet(agg, design = ~ batch + age + species) 
   colnames(dsq) <- agg$sample
   
-  colData(dsq)$species <- factor(colData(dsq)$species,
-                                   levels = c("mmus", "mcas", "mspr", "mcar"))
-  colData(dsq)$age <- factor(colData(dsq)$age, levels = c("yng", "old"))
+  # factor columns
+  colData(dsq)$species <- factor(
+    colData(dsq)$species,
+    levels = c("mmus", "mcas", "mspr", "mcar"))
+  colData(dsq)$age <- factor(
+    colData(dsq)$age,
+    levels = c("yng", "old"))
   colData(dsq)$Antibody_combination <- factor(
     colData(dsq)$Antibody_combination,
     levels = unique(colData(dsq)$Antibody_combination))
-  colData(dsq)$sample <- factor(colData(dsq)$sample,
-                                levels = unique(colData(dsq)$sample))
+  colData(dsq)$sample <- factor(
+    colData(dsq)$sample,
+    levels = unique(colData(dsq)$sample))
   
   return(dsq)
 })
 
 #-------------------------------------------------------------------------------
-saveRDS(dsq_list, file = snakemake@output[["deseq_output"]])
+base::saveRDS(dsq_list, file = snakemake@output[["deseq_output"]])
 
-sessionInfo()
+utils::sessionInfo()

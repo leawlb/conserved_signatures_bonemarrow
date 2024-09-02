90ef7edc586b00cd6294614053ccea730bfcc5aa
leawlb <lea.woelbert@gmail.com>
2024-09-02 14:46:03 +0200

    added functions for permutation tests on own datasets

--- code/source/sce_functions_reclustering.R
+++ code/source/sce_functions_reclustering.R
@@ -1,7 +1,18 @@
+
+# functions used for re-clustering of or permutation tests for own datasets,
+# or other datasets using different gene sets
+
 #-------------------------------------------------------------------------------
-# function for a standard seurat pipeline clustering pipeline from counts
-# make it so that mapply can be used for list of different resolution(s)
-# used for reclustering, and for reclustering permutation
+#-------------------------------------------------------------------------------
+
+######################### standard_seu_pipeline ################################
+
+#-------------------------------------------------------------------------------
+
+# function for a standard seurat pipeline clustering pipeline from raw counts
+# or, if counts are not available, starting at scaling normalised counts
+
+# designed so that mapply can be used for a list of different resolution(s)
 
 standard_seu_pipeline <- function(resolution, 
                                   features, 
@@ -10,9 +21,10 @@ standard_seu_pipeline <- function(resolution,
                                   calc_umap = FALSE){
   
   seu <- seu # seurat object
-  features <- features # features for re-clustering (all Features(seu))
+  features <- features # features for (re-)clustering 
   resolution <- resolution # resolution for FindClusters
-  data_use <- data_use # assay to use as basis for clustering
+  data_use <- data_use # assay to use as basis for clustering, either raw_counts
+                       # or log_counts
   calc_umap <- calc_umap # whether umap coordinates should be calculated
   
   print(resolution)
@@ -20,9 +32,6 @@ standard_seu_pipeline <- function(resolution,
   print("......starting Seurat pipeline")
   
   if(data_use == "raw_counts"){
-    
-    # remove data slot as counts from counts slot are being used
-    #seu@assays$RNA@data <- NULL
     
     # NormalizeData normalizes "count data"
     seu <- Seurat::NormalizeData(
@@ -43,6 +52,7 @@ standard_seu_pipeline <- function(resolution,
     verbose = FALSE, 
     features = features)
   
+  # in case the number of PCs is lower than 30 due to small datasets
   if(ncol(seu@reductions$pca) == 30){
     nr_pca <- 30
   }else if(ncol(seu@reductions$pca) < 30){
@@ -79,20 +89,82 @@ standard_seu_pipeline <- function(resolution,
   return(seu)
 }
 
-#-----------------------------------------------------------------------------
 
 
-#-----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
-# calculate chosen scores for evaluating reclustering
+############################## clustering_orig #################################
 
-calculate_scores_long <- function(
-    
+#-------------------------------------------------------------------------------
+
+# original clustering pipeline as used for our datasets
+# used here for re-clustering own datasets as part of permutation and 
+# gene set testing
+
+clustering_orig <- function(sce, k_graph, resolution_louvain){
+  
+  sce <- sce # SCE object (own dataset)
+  k_graph <- k_graph # k = number of nearest neighbors for buildSNNGraph()
+  resolution_louvain <- resolution_louvain # resolution for cluster_louvain()
+  
+  print(nrow(sce))
+  print(ncol(sce))
+  
+  # re-calculate PCA (without batch correction)
+  # use logcounts assay, contains normalized logcounts from multibatchnorm
+  # remove old PCA
+  reducedDims(sce)$PCA <- NULL
+  sce <- scater::runPCA(sce, 
+                        ncomponents = 25, 
+                        exprs_values = "logcounts") 
+  
+  # get a graph of nearest neighbors
+  graph <- scran::buildSNNGraph(sce, 
+                                k = k_graph, 
+                                use.dimred = "PCA", 
+                                type = "rank")
+  
+  # community detection
+  clust <- igraph::cluster_louvain(graph, resolution = resolution_louvain)
+  
+  # add to sce object 
+  sce$reclustered <- clust$membership
+  
+  sce$reclustered <- base::factor(
+    sce$reclustered, 
+    levels = base::sort(base::unique(sce$reclustered)))
+  print(base::table(sce$reclustered, sce$celltypes))
+  
+  sce$k_graph <- base::rep(k_graph, ncol(sce))
+  sce$resolution_louvain <- base::rep(resolution_louvain, ncol(sce))
+  
+  return(sce)
+}
+
+
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+############################## calculate_scores ################################
+
+#-------------------------------------------------------------------------------
+
+# calculate chosen scores for evaluating re-clustering
+
+calculate_scores <- function(
     seu = NULL, 
-    cluster_vector1 = NULL,
-    cluster_vector2 = NULL,
-    mat_pca = NULL,
-    for_permutation = FALSE){
+    cluster_vector1 = NULL, 
+    cluster_vector2 = NULL, 
+    mat_pca = NULL, 
+    for_permutation = FALSE){ 
+  
+  seu <- seu # seurat object to be evaluated
+  cluster_vector1 <- cluster_vector1 # or, a vector of original clusters 
+  cluster_vector2 <- cluster_vector2 # and of new clusters for comparison
+  mat_pca <- mat_pca # and a matrix of PC coordinates
+  for_permutation <- for_permutation # whether used for perm or not
   
   # can be seurat object, or two clustering vectors + pca matrix
   
@@ -122,6 +194,7 @@ calculate_scores_long <- function(
   nr_celltypes <- nrow(mat)
   nr_clusters <- ncol(mat)
   
+  #-----------------------------------------------------------------------------
   #-----------------------------------------------------------------------------
   # score 1: proportions of 0
   
@@ -186,7 +259,7 @@ calculate_scores_long <- function(
   #-----------------------------------------------------------------------------
   #-----------------------------------------------------------------------------
   
-  # depending on what this function is used for, output differs slightly
+  # depending on what this function is used for, output differs 
   if(!for_permutation){
     
     return_df <- base::data.frame(
@@ -223,43 +296,50 @@ calculate_scores_long <- function(
   return(return_df)
 }  
 
-#-------------------------------------------------------------------------------
-
-
-
 
 
 #-------------------------------------------------------------------------------
-# function that combines two previous functions to go through i
-# iterations of seurat re-clustering using i sets of random genes and
-# calculating the i sets of resulting scores
-# the function will only output the scores for direct comparison 
+#-------------------------------------------------------------------------------
 
-random_reclustering_scores <- function(iteration, 
-                                       seu, 
-                                       iteration_df, 
-                                       resolution,
-                                       data_use){
+#################### permuting_reclustering_scores_seurat ######################
+
+#-------------------------------------------------------------------------------
+
+# function that combines two functions, one for re-clustering, one for 
+# calculating scores
+
+# used for permutation test with n iterations 
+# used for seurat objects only
+# the function will only output the scores for direct comparison, but not 
+# the re-clustered datasets.
+
+permuting_reclustering_scores_seurat <- function(
+    iteration, 
+    seu, 
+    iteration_df, 
+    resolution,
+    data_use){
+  
+  print("starting iterations")
   
   iteration <- iteration # list of iterations like: as.list(c(1:i))
   seu <- seu # seurat object to be reclustered 
-  iteration_df <- iteration_df # dataframe with ncol=i and random sets of gene positions in each column
+  iteration_df <- iteration_df # df with ncol=i and random gene positions/col
   resolution <- resolution # resolution to parse to FindClusters
-  data_use <- data_use # the assay to be used for reclustering ("RNA" in my case)
+  data_use <- data_use # data for reclustering ("log_counts" or "raw_counts")
   
   # subset object to previously generated random list of genes from iteration_df
   iteration_vector <- iteration_df[,iteration]
-  #print(iteration_vector[1:10])
-  
+
   seu_sub <- BiocGenerics::subset(
     seu,
     features = SeuratObject::Features(seu)[iteration_vector], 
     slot = "count")
+  # check that GENES were subsetted
   print(nrow(seu_sub))
-  print(nrow(iteration_df))
-  
+
   print("starting standard_seu_pipeline function")
-  # re-cluster seurat object from raw counts, using basic seurat pipeline
+  # re-cluster seurat objects from using standard seurat pipeline
   seu_rec <- standard_seu_pipeline(
     seu = seu_sub, 
     features = SeuratObject::Features(seu_sub), 
@@ -267,13 +347,78 @@ random_reclustering_scores <- function(iteration,
     data_use = data_use,
     calc_umap = FALSE)
 
-  print("starting calculate_scores_long function")
-  # calculate re-clustering scores as defined above
-  res_df <- calculate_scores_long(seu_rec, for_permutation = TRUE)
+  print("starting calculate_scores function")
+  # calculate re-clustering scores 
+  res_df <- calculate_scores(seu_rec, for_permutation = TRUE)
   res_df$iteration[1] <- iteration
   
   return(res_df)
 }
 
 
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+##################### permuting_reclustering_scores_sce ########################
+
+#-------------------------------------------------------------------------------
+
+# function that combines two functions, one for re-clustering, one for 
+# calculating scores
+
+# used for permutation test with n iterations 
+# used for SCE objects (own datasets) only
+# the function will only output the scores for direct comparison, but not 
+# the re-clustered datasets.
+
+permuting_reclustering_scores_sce <- function(
+  iteration, 
+  sce, 
+  iteration_df, 
+  resolution_louvain,
+  k_graph){
+  
+  print("starting iteration")
+  
+  iteration <- iteration # list of iterations like: as.list(c(1:i))
+  sce <- sce # sce object (own dataset) to be reclustered 
+  iteration_df <- iteration_df # df with ncol=i and random gene positions/col 
+  resolution_louvain <- resolution_louvain # resolution for louvain clustering
+  k_graph <- k_graph # number of k for SNNgraph
+  
+  # subset object to previously generated random list of genes from iteration_df
+  iteration_vector <- iteration_df[,iteration]
+
+  # subset genes (rows)
+  sce_sub <- sce[iteration_vector,]
+  
+  # make sure that only GENES have been subsetted, not cells
+  print(nrow(sce))
+  print(ncol(sce))
+  
+  print("starting reclustering function")
+  # re-cluster sce objects using original pipeline
+  seu_rec <- clustering_orig(
+    sce = sce_sub,
+    k_graph = k_graph,
+    resolution_louvain = resolution_louvain
+  )
+  
+  print("starting calculate_scores function")
+  # calculate re-clustering scores 
+  
+  # define input
+  cluster_vector1 <- seu_rec$celltypes
+  cluster_vector2 <- seu_rec$reclustered
+  mat_pca <- SingleCellExperiment::reducedDims(seu_rec)$PCA[,1:10]
+  
+  res_df <- calculate_scores( 
+    cluster_vector1 = cluster_vector1,
+    cluster_vector2 = cluster_vector2,
+    mat_pca = mat_pca, 
+    for_permutation = TRUE)
+  
+  return(res_df)
+}
 

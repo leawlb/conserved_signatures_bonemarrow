fadc2ccbd08bae02e28ea2af15252615e7090b6c
leawlb <lea.woelbert@gmail.com>
2025-02-24 09:25:31 +0100

    working on figures

---
+++ code/figures/run_rmds.py
@@ -0,0 +1,56 @@
+
+# output for htmls
+# doesn't have to be base
+OUTPUT_PATH = "/omics/odcf/analysis/OE0538_projects_temp/DO-0008/data_temp/micromamba_test_rerun/data/manuscript1/htmls"
+
+# input and output paths of data and separate plots are separarely defined
+# in each .Rmd script for better overview within the script
+
+#-------------------------------------------------------------------------------
+
+targets = []
+
+targets = targets + [OUTPUT_PATH + "/figure1.html"]
+targets = targets + [OUTPUT_PATH + "/figure5.html"]
+targets = targets + [OUTPUT_PATH + "/figure6.html"]
+
+#-------------------------------------------------------------------------------
+
+localrules: all  
+
+rule all: 
+  input:
+      targets
+
+rule run_fig1:
+    resources:
+        mem_mb=80000,
+        queues = "medium"
+    output:
+        OUTPUT_PATH + "/figure1.html",
+    threads: 4
+    script:
+        "figure1.Rmd"
+        
+rule run_fig5:
+    resources:
+        mem_mb=80000,
+        queues = "medium"
+    output:
+        OUTPUT_PATH + "/figure5.html",
+    threads: 4
+    script:
+        "figure5.Rmd"
+
+
+rule run_fig6:
+    resources:
+        mem_mb=80000,
+        queues="medium"
+    output:
+        OUTPUT_PATH + "/figure6.html"
+    threads: 4
+    conda:
+        "../envs/upsetplot.yml"
+    script:
+        "figure6.Rmd"

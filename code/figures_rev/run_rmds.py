f86fa62bf40b5ceabab769585020c39110551417
leawlb <lea.woelbert@gmail.com>
2025-12-02 14:01:08 +0100

    working on custom clustering supp figure

---
+++ code/figures_rev/run_rmds.py
@@ -0,0 +1,35 @@
+
+# output for htmls
+# doesn't have to be base
+OUTPUT_PATH = "/omics/odcf/analysis/OE0538_projects/DO-0008/data/manuscript1_rev/htmls"
+
+# input and output paths of data and separate plots are separarely defined
+# in each .Rmd script for better overview within the script
+
+# all plots geneated using this script are just parts of figures and details
+# (but not data) are manually adjusted using Affinity.
+
+#-------------------------------------------------------------------------------
+
+targets = []
+
+targets = targets + [OUTPUT_PATH + "/figure_s3.html"]
+
+#-------------------------------------------------------------------------------
+
+localrules: all
+
+rule all: 
+  input:
+      targets
+
+rule run_fig_s3:
+    resources:
+        mem_mb=80000,
+        queues="medium-debian"
+    output:
+        OUTPUT_PATH + "/figure_s3.html"
+    threads: 4
+    script:
+        "figure_s3.Rmd"
+        
